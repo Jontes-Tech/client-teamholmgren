c2ad1bce015f4251486ec5ce5ba17f90026b3f00
Jontes-Tech <jonatan@jontes.page>
2022-11-09 18:39:26 +0100

    fix: spacing and newlines

--- src/lib/pages/Contact.svelte
+++ src/lib/pages/Contact.svelte
@@ -4,14 +4,13 @@
 
 <main class="font-jost hyphens-manual flex-grow pb-12 m-10">
   <p class="text-white whitespace-pre-line ">
-    {$_("billingAdress")}: TEAM Holmgren Consulting AB Rankhyttan 50 791 92
-    Falun
+    {$_("billingAdress")}: 
+      TEAM Holmgren Consulting AB
+      Rankhyttan 50 791 92
+      Falun
 
     {$_("organisationNumber")}: 559404-6798
-
-    <!-- TODO: First, replace the x:es with the VAT number, then uncomment the code and lastly remove this comment. -->
-    <!-- Moms/VAT {$_('number')}: XXXXXXXX -->
-
+    
     {$_("phoneNum")}:
     <a class="no-underline hover:underline font-bold" href="tel:46705429002"
       >+46 70 542 90 02</a
